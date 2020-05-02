3889fbb942b99d2457db06a4f5b2980161ca3a7d
Lucas Jellema <lucas.jellema@amis.nl>
2020-05-02 12:52:17 +0200

    Slides and scripts for WindowsSandbox

---
+++ WindowsSandbox/DBeaverDownloadAndInstall.cmd
@@ -0,0 +1,8 @@
+REM Download DBeaver Community Edition 
+curl -L "https://dbeaver.io/files/dbeaver-ce-latest-x86_64-setup.exe" --output C:\users\WDAGUtilityAccount\Desktop\dbeaver-ce-latest-x86_64-setup.exe
+ 
+REM Install DBeaver CE
+C:\users\WDAGUtilityAccount\Desktop\dbeaver-ce-latest-x86_64-setup.exe /S /currentuser
+
+REM Run DBeaver CE
+C:\Users\WDAGUtilityAccount\AppData\Local\DBeaver\dbeaver.exe
---
+++ WindowsSandbox/VSCodeInstall.cmd
@@ -0,0 +1,5 @@
+REM Download VSCode
+curl -L "https://update.code.visualstudio.com/latest/win32-x64-user/stable" --output C:\users\WDAGUtilityAccount\Desktop\vscode.exe
+ 
+REM Install and run VSCode
+C:\users\WDAGUtilityAccount\Desktop\vscode.exe /verysilent /suppressmsgboxes
